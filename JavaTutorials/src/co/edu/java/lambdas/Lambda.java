91a72b899f8558498f37917dddcd9e3e78d7b580
Juan Carlos <juancr82@gmail.com>
2016-02-22 19:10:18 -0500

    Starting with lambda expressions.

---
+++ JavaTutorials/src/co/edu/java/lambdas/Lambda.java
@@ -0,0 +1,31 @@
+package co.edu.java.lambdas;
+
+import java.util.Arrays;
+
+public class Lambda {
+
+	public static void main(String[] args) {
+		
+		Arrays.asList( "a", "b", "d" ).forEach( e -> System.out.println( e ) );
+		
+		Arrays.asList( "a", "b", "d" ).forEach( ( String e ) -> System.out.println( e ) );
+		
+		Arrays.asList( "a", "b", "d" ).forEach( e -> {
+		    System.out.print( e );
+		    System.out.print( e );
+		} );
+		
+		String separator = ",";
+		Arrays.asList( "a", "b", "d" ).forEach( 
+		    ( String e ) -> System.out.print( e + separator ) );
+		
+		Arrays.asList( "a", "b", "d" ).sort( ( e1, e2 ) -> e1.compareTo( e2 ) );
+		
+		Arrays.asList( "a", "b", "d" ).sort( ( e1, e2 ) -> {
+		    int result = e1.compareTo( e2 );
+		    return result;
+		} );
+		
+	}
+
+}
